9e3fb0c1a198c511661e82ad5b3fe2c136e5cd46
Ozair Zaman <48018195+OzairZaman@users.noreply.github.com>
2019-03-11 16:04:05 +1100

    new version of Ass

---
+++ NewAss'tRoids/Assets/Scripts/Game/Asteroid/AsteroidManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidManager : MonoBehaviour {
+
+    //variables
+    public GameObject[] AsteroidsPrefabs;
+    public float maxVelocity; //max velocity we will give our asteroid (weuse it to influence [Random]
+    public float spawnRate;
+    public float spawnPadding;
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //spans an (a single) asteroid at the random location with random force
+    public void Spawn(GameObject prefab, Vector3 position)
+    {
+        //randomize the rotation of the asteroid
+        Quaternion randomRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 30f));
+
+        //spawn random asteroid at random positoin and default Quaternionf
+        GameObject asteroid = Instantiate(prefab, position, randomRotation, this.transform);
+
+        //get rigid body from asteroid 
+        Rigidbody2D rigid = asteroid.GetComponent<Rigidbody2D>();
+
+        //apply random force to rigid body
+        Vector2 randomForce = Random.insideUnitCircle * maxVelocity;
+        rigid.AddForce(randomForce, ForceMode2D.Impulse);
+    }
+
+    //
+}
---
+++ NewAss'tRoids/Assets/Scripts/Game/Player/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //variables
+    public float movementSpeed = 10f;
+    public float rotationSpeed = 230f;
+    private Rigidbody2D rigid;
+    
+    
+    
+    // Use this for initialization
+	void Start () {
+        rigid = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Control();
+	}
+
+    void Control()
+    {
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+        //rotate to left
+        this.transform.Rotate(Vector3.forward, -inputHorizontal * rotationSpeed * Time.deltaTime);
+        //forward backwards
+        rigid.AddForce(inputVertical*transform.up*movementSpeed);
+    }
+}
